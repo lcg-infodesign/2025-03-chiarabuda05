e98e24ce2fce5d8804a4d9788eb72437de319ce6
chiarabuda05 <chiara.buda@mail.polimi.it>
2025-11-02 16:12:53 +0100

    .

--- sketch.js
+++ sketch.js
@@ -1,17 +1,57 @@
+
+let data;
+let miniLat, miniLon, maxiLat, maxiLon;
+
+
 function preload() {
   // put preload code here
+  data = loadTable ("assets/data.csv", "csv", "header");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
-}
 
+
+  let allLat=data.getColumn("Latitude");
+  miniLat=min(allLat);
+  maxiLat=max(allLat);
+
+  let allLon = data.getColumn("Longitude");
+  miniLon = min(allLon);
+  maxiLon = max(allLon);
+
+  console.log (miniLat, maxiLat);
+
+}
 function draw() {
   // put drawing code here
-}
+  background (10);
+
+  for (let rownumber=0; rownumber<data.getRowCount(); rownumber++){
+    console.log (rownumber);
+
+    let lat = data.getNum(rownumber, "Latitude");
+    let lon = data.getNum(rownumber, "Longitude");
+
+    let x = map (lon, miniLon, maxiLon, 0, width);
+    let y = map (lat, miniLat, maxiLat, height, 0);
+    let radius =10;
+    let d = dist(x, y, mouseX, mouseY);
+
+    if (d<radius) {
+    fill("red"); }
+
+
+    if (d>radius) {
+    fill("yellow"); }
+    
+    ellipse (x,y, radius*2);
+
+    //calcolo la distanza 
+   
+  
+  }
+
+
+  }
